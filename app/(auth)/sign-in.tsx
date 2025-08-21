11a0fdfe813906d4ce6edee679b75274a9ffc2a4
Salomon Uwimana Masasu <113304910+Masasu74@users.noreply.github.com>
2025-08-21 19:25:27 +0200

    a

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,14 +1,60 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-import { router } from 'expo-router'
+import { View, Text, Button, Alert } from "react-native";
+import React, { useState } from "react";
+import { Link, router } from "expo-router";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton"
 
 const SignIn = () => {
-  return (
-    <View>
-      <Text>SignUp</Text>
-      <Button title="Sign In" onPress={()=>router.push("/sign-up")}/>
-    </View>
-  )
-}
+  const [isSubmitting,setIsSubmitting]=useState(false);
+  const [form,setForm]=useState({email:'',password:''});
 
-export default SignIn
+  const submit=async()=>{
+    if(!form.email || !form.password) Alert.alert('Error','Please enter valid email & password');
+
+    setIsSubmitting(true)
+
+    try {
+      Alert.alert('success','User signed in successfully.');
+      router.replace('/');
+      
+    } catch (error:any) {
+      Alert.alert('Error',error.message)
+    } finally{
+      setIsSubmitting(false)
+    }
+  }
+
+
+  return (
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+
+      <CustomInput
+        placeholder="Enter your email"
+        value={""}
+        onChangeText={(text) => {}}
+        label="Email"
+        keyboardType="email-address"
+      />
+       <CustomInput
+        placeholder="Enter your password"
+        value={""}
+        onChangeText={(text) => {}}
+        label="Password"
+        secureTextEntry={true}
+      />
+      <CustomButton 
+      title="Sign In"/>
+
+      <View className="flex justify-center mt-5 flex-row gap-2">
+        <Text className="base-regular text-gray-100">
+          Don't have an account?
+        </Text>
+        <Link href="/sign-up" className="base-bold text-primary">
+        Sign Up
+        </Link>
+      </View>
+    </View>
+  );
+};
+
+export default SignIn;
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig={
     endpoint:process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     platform:"com.jsm.foodordering",
-    projectId:process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+    projectId:process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId:'68a5b4b30027ed55524e',
+    userCollectionId:'68a5b55a002563310afe'
 }

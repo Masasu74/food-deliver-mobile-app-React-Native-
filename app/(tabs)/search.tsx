06cbc9faf549920dbd992319d9000e5c2ecb62b3
Salomon Uwimana Masasu <113304910+Masasu74@users.noreply.github.com>
2025-08-26 15:14:19 +0200

    a

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,7 +1,26 @@
+import { getCategories, getMenu } from '@/lib/appwrite'
+import useAppwrite from '@/lib/useAppwrite'
+import { useLocalSearchParams } from 'expo-router'
+import { useEffect } from 'react'
 import { View, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const search = () => {
+  const {category,query}=useLocalSearchParams<{query:string;category:string}>()
+  const{data,refetch,loading}=useAppwrite({
+    fn:getMenu,
+    params:{
+      category,
+      query,
+      limit:6
+    }
+  });
+const {data:categories}=useAppwrite({fn:getCategories});
+   
+useEffect(()=>{
+  refetch({category,query,limit:6})
+},[category,query]);
+
   return (
    <SafeAreaView>
    <Text>Search</Text>

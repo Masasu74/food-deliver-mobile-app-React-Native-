7e09bfc97e39598482b712ef2ad3d57b0e385181
Salomon Uwimana Masasu <113304910+Masasu74@users.noreply.github.com>
2025-08-28 19:10:10 +0200

    Search Screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,31 +1,66 @@
-import { getCategories, getMenu } from '@/lib/appwrite'
-import useAppwrite from '@/lib/useAppwrite'
-import { useLocalSearchParams } from 'expo-router'
-import { useEffect } from 'react'
-import { View, Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-
+import CartButton from "@/components/CartButton";
+import { getCategories, getMenu } from "@/lib/appwrite";
+import useAppwrite from "@/lib/useAppwrite";
+import { useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
+import { View, Text, FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import cn from 'clsx'
 const search = () => {
-  const {category,query}=useLocalSearchParams<{query:string;category:string}>()
-  const{data,refetch,loading}=useAppwrite({
-    fn:getMenu,
-    params:{
+  const { category, query } = useLocalSearchParams<{
+    query: string;
+    category: string;
+  }>();
+  const { data, refetch, loading } = useAppwrite({
+    fn: getMenu,
+    params: {
       category,
       query,
-      limit:6
-    }
+      limit: 6,
+    },
   });
-const {data:categories}=useAppwrite({fn:getCategories});
-   
-useEffect(()=>{
-  refetch({category,query,limit:6})
-},[category,query]);
+  const { data: categories } = useAppwrite({ fn: getCategories });
+
+  useEffect(() => {
+    refetch({ category, query, limit: 6 });
+  }, [category, query]);
 
   return (
-   <SafeAreaView>
-   <Text>Search</Text>
-   </SafeAreaView>
-  )
-}
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={data}
+        renderItem={({ item, index }) => {
+          const isFirstRightColItem=index%2===0;
+           
+          return (
+            <View className={cn("flex-1 max-w-[48%]",!isFirstRightColItem ? 'mt-10':'mt-0')}>
+              <Text>Menu Card</Text>
+            </View>
+          );
+        }}
+        keyExtractor={Item =>Item.$id}
+        numColumns={2}
+        columnWrapperClassName="gap-7"
+        contentContainerClassName="gap-7 px-5 pb-32"
+        ListHeaderComponent={()=>(
+          <View className="my-5 gap-5">
+              <View className="flex-between flex-row w-full">
+                <View className="flex-start">
+                  <Text className="small-bold uppercase text-primary">Search</Text>
+                  <View className="flex-start flex-row gap-x-1 mt-0.5">
+                    <Text className="paragraph-semibold text-dark-100">Find your favorite food</Text>
+                  </View>
+                </View>
+                <CartButton/>
+              </View>
+              <Text>Search Input</Text>
+              <Text>Filter</Text>
+          </View>
+        )}
+        ListEmptyComponent={()=>!loading && <Text>No Results</Text>}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default search
+export default search;
